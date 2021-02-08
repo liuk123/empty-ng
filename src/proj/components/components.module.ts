0fc759d77a4cbade7b7c05f38ebdcac758fe57b8
liuk123 <40844000+liuk123@users.noreply.github.com>
2021-02-08 21:54:27 +0800

    简单修改

--- src/proj/components/components.module.ts
+++ src/proj/components/components.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuBoxComponent } from './menu-box/menu-box.component';
 import { ComponentsRoutingModule } from './components-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [MenuBoxComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     ComponentsRoutingModule
   ]
 })
