238dc39b6ea8bdb581f2b3ae1bd84d50c0e9cd14
liuk123 <40844000+liuk123@users.noreply.github.com>
2021-03-04 09:37:33 +0800

    123321

--- src/proj/drop-drag/directive/drag.directive.ts
+++ src/proj/drop-drag/directive/drag.directive.ts
@@ -1,12 +1,24 @@
 import { Directive, HostListener, ElementRef, Renderer2, Input } from '@angular/core';
-
+export class DragData{
+  constructor(
+    public left?:number,
+    public top?:number,
+  ){
+    if(!left){
+      this.left=0
+    }
+    if(!top){
+      this.top=0
+    }
+  }
+}
 @Directive({
   selector: '[ins-draggable]',
 })
 export class DragDirective {
   private _isDraggable = false;
   @Input() draggedClass: string;
-  @Input() dragData: any;
+  @Input() dragData: DragData = new DragData();
   @Input('ins-draggable')
   set isDraggable(draggable: boolean) {
     this._isDraggable = draggable;
@@ -16,24 +28,40 @@ export class DragDirective {
     return this._isDraggable;
   }
 
+  x: number
+  y: number
   constructor(
-    private el:ElementRef,
-    private rd:Renderer2,) { }
+    private el: ElementRef,
+    private rd: Renderer2,) { }
 
-    @HostListener('dragstart', ['$event'])//监听事件
-    onDragStart(ev: Event) {
-      if(this.el.nativeElement===ev.target){
-        console.log('dragstart')
-        this.rd.addClass(this.el.nativeElement, this.draggedClass);
-      }
+  @HostListener('dragstart', ['$event'])
+  onDragStart(ev) {
+    if (this.el.nativeElement === ev.target) {
+      this.x = ev.clientX
+      this.y = ev.clientY
+      console.log(this.x)
+      this.rd.setStyle(this.el.nativeElement, 'opacity', '0.4')
     }
-  
-    @HostListener('dragend', ['$event'])
-    onDragEnd(ev: Event) {
-      if (this.el.nativeElement === ev.target) {
-        console.log('dragend')
-        this.rd.removeClass(this.el.nativeElement, this.draggedClass);
-      }
+  }
+
+  @HostListener('drag', ['$event'])
+  onDrag(ev: Event) {
+    if (this.el.nativeElement === ev.target) {
+      // console.log('drag')
     }
+  }
+
+  @HostListener('dragend', ['$event', '$event.target'])
+  onDragEnd(ev,target) {
+    if (this.el.nativeElement === ev.target) {
+      console.log(target)
+      this.dragData.left = this.dragData.left + ev.clientX - this.x
+      this.dragData.top = this.dragData.top + ev.clientY - this.y
+      this.rd.setStyle(this.el.nativeElement, 'left', this.dragData.left + 'px')
+      this.rd.setStyle(this.el.nativeElement, 'top', this.dragData.top + 'px')
+      this.rd.setStyle(this.el.nativeElement, 'opacity', '1')
+      // this.rd.removeClass(this.el.nativeElement, this.draggedClass);
+    }
+  }
 
 }
--- src/proj/drop-drag/directive/drop.directive.ts
+++ src/proj/drop-drag/directive/drop.directive.ts
@@ -4,17 +4,26 @@ import { Directive, HostListener, ElementRef, Renderer2, Input} from '@angular/c
   selector: '[ins-droppable]',
 })
 export class DropDirective {
-  @Input('dragEnterClass') dragEnterClass:string='';
+  @Input('dragEnterClass') dragEnterClass:string='adsd';
   constructor(
     private el:ElementRef,
-    private rd:Renderer2,) {console.log(1233)}
-
+    private rd:Renderer2,) {
+       
+    }
+//设置dom元素属性
+// if (this.dropTags.indexOf(dragData.tag) > -1) {
+//   this.rd.setProperty(ev, 'dataTransfer.effectAllowed', 'all');
+//   this.rd.setProperty(ev, 'dataTransfer.dropEffect', 'move');
+// } else {
+//   this.rd.setProperty(ev, 'dataTransfer.effectAllowed', 'none');
+//   this.rd.setProperty(ev, 'dataTransfer.dropEffect', 'none');
+// }
   @HostListener('dragenter', ['$event'])
   onDragEnter(ev: Event) {
     ev.preventDefault();
     ev.stopPropagation();
     if (this.el.nativeElement === ev.target) {
-      console.log('dragenter');
+      // console.log('dragenter');
       this.rd.addClass(this.el.nativeElement, this.dragEnterClass);
     }
   }
@@ -24,7 +33,7 @@ export class DropDirective {
     ev.preventDefault();
     ev.stopPropagation();
     if (this.el.nativeElement === ev.target) {
-      console.log('dragover')
+      // console.log('dragover')
     }
   }
 
@@ -33,7 +42,7 @@ export class DropDirective {
     ev.preventDefault();
     ev.stopPropagation();
     if (this.el.nativeElement === ev.target) {
-      console.log('dragleave');
+      // console.log('dragleave');
       this.rd.removeClass(this.el.nativeElement, this.dragEnterClass);
     }
   }
