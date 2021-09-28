7e7830e051d08631a6f43f3a467173f012b2f123
liuk123 <40844000+liuk123@users.noreply.github.com>
2021-09-28 16:33:35 +0800

    1

--- src/proj/blog/blog.module.ts
+++ src/proj/blog/blog.module.ts
@@ -8,40 +8,14 @@ import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 import { ArticleService } from './services/article.service';
 import { CommentService } from './services/comment.service';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
-// import { MarkdownModule, MarkedOptions, MarkedRenderer } from 'ngx-markdown';
 import { OperateListComponent } from './my-blog/operate-list/operate-list.component';
 import { MyBlogComponent } from './my-blog/my-blog.component';
-
-// export function markedOptionsFactory(): MarkedOptions {
-//   const renderer = new MarkedRenderer();
-
-//   renderer.blockquote = (text: string) => {
-//     return '<blockquote class="blockquote"><p>' + text + '</p></blockquote>';
-//   };
-
-//   return {
-//     renderer: renderer,
-//     gfm: true,
-//     breaks: false,
-//     pedantic: false,
-//     smartLists: true,
-//     smartypants: false,
-//   };
-// }
 
 @NgModule({
   declarations: [ArtListComponent,BlogHomeComponent,TagsComponent,BlogDetailComponent,BlogEditComponent,OperateListComponent,MyBlogComponent],
   imports: [
     SharedModule,
     BlogRoutingModule,
-    // MarkdownModule.forRoot({ 
-    //   loader: HttpClient,
-    //   sanitize: SecurityContext.NONE,
-    //   markedOptions: {
-    //     provide: MarkedOptions,
-    //     useFactory: markedOptionsFactory,
-    //   },
-    // }),
   ],
   providers:[ArticleService,CommentService]
 })
