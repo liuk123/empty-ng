bb0847a1fb166385dbf7091693962c711b09f15b
liuk123 <40844000+liuk123@users.noreply.github.com>
2021-11-25 17:15:10 +0800

    分类修改

--- src/proj/blog/setting/setting.component.ts
+++ src/proj/blog/setting/setting.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Form, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from 'src/app/biz/services/common/user.service';
+import { MessageUtilService } from 'src/app/core/services/message-util.service';
 import { ArticleService } from '../services/article.service';
 
 @Component({
@@ -11,34 +13,94 @@ export class SettingComponent implements OnInit {
 
   categoryValue = null
   validateForm!: FormGroup;
+  categoryData = null
+  userInfo = null
 
+  get categoryList(){
+    return this.validateForm.get('categoryList') as FormArray
+  }
+  
   constructor(
     private fb: FormBuilder,
-    private srv: ArticleService) {}
+    private srv: ArticleService,
+    private userSrv:UserService,
+    private message: MessageUtilService,) {}
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       categoryList: this.fb.array([])
     });
+
+    this.userSrv.userEvent.subscribe(v=>{
+      if(v.id){
+        this.userInfo = v
+        this.getCategory(v.id).subscribe(res => {
+          if(res.isSuccess()){
+            this.categoryData = res.data
+            this.categoryData.forEach(v=>{
+              this.categoryList.push(this.fb.group({
+                id: this.fb.control(v.id),
+                name: this.fb.control(v.name)
+              }))
+            })
+          }
+        })
+      }
+    })
   }
-  get categoryList(){
-    return this.validateForm.get('categoryList') as FormArray
-  }
+  /**
+   * 添加分类
+   * @returns 
+   */
   addCategory(): void {
     if(!this.categoryValue){
       return null
     }
-    this.categoryList.push(this.fb.group({
-      id: this.fb.control(123),
-      value: this.fb.control(this.categoryValue)
-    }))
-    this.categoryValue = null
+    const params={
+      name: this.categoryValue
+    }
+    this.srv.saveCategory(params).subscribe(res=>{
+      if(res.isSuccess()){
+        this.categoryList.push(this.fb.group({
+          id: this.fb.control(null),
+          name: this.fb.control(this.categoryValue)
+        }))
+        this.categoryValue = null
+        this.message.success('添加分类成功。')
+      }
+    })
+    
   }
+  /**
+   * 删除分类
+   * @param i 
+   * @param data 
+   */
   removeCategory(i, data): void {
-    this.categoryList.removeAt(i)
-    console.log(data)
+    this.srv.delCategory(data.id).subscribe(res=>{
+      if(res.isSuccess()){
+        this.categoryList.removeAt(i)
+        this.message.success('删除分类成功。')
+      }
+    })
   }
-  editCategory(data): void {
-    console.log(data)
+  /**
+   * 修改分类
+   * @param data 
+   */
+  editCategory(data: {id:number, name:string}): void {
+    this.srv.saveCategory(data).subscribe(res=>{
+      if(res.isSuccess()){
+        this.message.success('修改分类成功。')
+      }
+    })
+  }
+  /**
+   * 获取分类
+   * @param id 
+   * @returns 
+   */
+  getCategory(id){
+    return this.srv.getCategory({id})
   }
 }
