38a458c1d1c3919a59784b1b8e9d1fed3f450207
liuk123 <40844000+liuk123@users.noreply.github.com>
2022-01-05 15:29:45 +0800

    数据格式转化

--- src/proj/navigation/navigation-custiom/navigation-custiom.component.ts
+++ src/proj/navigation/navigation-custiom/navigation-custiom.component.ts
@@ -18,7 +18,7 @@ export class NavigationCustiomComponent implements OnInit {
   customNavs
   customData
   selectData: Navigation[][] = []
-  selectTitle:string = null
+  selectTitle: string = null
   constructor(
     private jsutil: JsUtilService,
     private util: UtilService,
@@ -37,73 +37,73 @@ export class NavigationCustiomComponent implements OnInit {
    * 打开新窗口
    * @param item 
    */
-  open(item: Navigation){
-    if(item.type === 'link'){
+  open(item: Navigation) {
+    if (item.type === 'link') {
       window.open(item.link, '_blank')
-    }else{
+    } else {
 
     }
   }
-  selectNav(id){
-    let tem = this.jsutil.findItem(this.customNavs, (data)=>data.id ==id)
-    if(tem){
+  selectNav(id) {
+    let tem = this.jsutil.findItem(this.customNavs, (data) => data.id == id)
+    if (tem) {
       this.selectTitle = tem.title
-      this.srv.getNavItem({pid:id}).subscribe(res=>{
-        if(res.isSuccess()){
+      this.srv.getNavItem({ pid: id }).subscribe(res => {
+        if (res.isSuccess()) {
           let arr
-          if(tem.children){
-            arr=[
-              ...res.data.map(v=>({...v,type:'link'})),
-              ...tem.children.map(v=>({...v, type: 'sub'}))
-            ]            
-          }else{
-            arr=res.data
+          if (tem.children) {
+            arr = [
+              ...res.data.map(v => ({ ...v, type: 'link' })),
+              ...tem.children.map(v => ({ ...v, type: 'sub' }))
+            ]
+          } else {
+            arr = res.data
           }
-          this.selectData = this.util.columnsArr(arr,3,1)
+          this.selectData = this.util.columnsArr(arr, 3, 1)
           this.cf.markForCheck()
         }
       })
-      
+
     }
   }
-  
+
 
   /**
    * 导航分类添加编辑
    * @param title 
    * @param data 
    */
-  showNavCategoryDialog(title, data={}){
-    let params=[
+  showNavCategoryDialog(title, data = {}) {
+    let params = [
       {
         key: 'id',
         label: 'id',
-        value: data['id']||null,
-        valide:[],
+        value: data['id'] || null,
+        valide: [],
         controlType: 'textbox',
         type: 'hidden',
-      },{
+      }, {
         key: 'title',
         label: '名称',
-        value: data['title']||null,
-        valide:[],
+        value: data['title'] || null,
+        valide: [],
         controlType: 'textbox',
         type: 'text',
-      },{
+      }, {
         key: 'sort',
         label: '排序',
-        value: data['sort']||null,
-        valide:[],
+        value: data['sort'] || null,
+        valide: [],
         controlType: 'textbox',
         type: 'text',
-      },{
+      }, {
         key: 'pid',
         label: '父级',
-        value: data['pid']?data['pid'].map(v=>v.id):null,
-        valide:[],
+        value: data['pid'] ? data['pid'].map(v => v.id) : null,
+        valide: [],
         controlType: 'dropdown',
         type: 'default',
-        options: this.customData.map(v=>({name: v.title, code:v.id}))
+        options: this.customData.map(v => ({ name: v.title, code: v.id }))
       }
     ]
     this.modal.create({
@@ -111,11 +111,11 @@ export class NavigationCustiomComponent implements OnInit {
       nzContent: FormGroupComponent,
       nzViewContainerRef: this.viewContainerRef,
       nzComponentParams: {
-        params:params,
+        params: params,
         span: 1,
       },
-      nzOnOk: (component:any) => {
-        this.srv.saveNavCategory([component.validateForm.value]).subscribe(v=>{
+      nzOnOk: (component: any) => {
+        this.srv.saveNavCategory([component.validateForm.value]).subscribe(v => {
 
         })
       }
@@ -126,113 +126,126 @@ export class NavigationCustiomComponent implements OnInit {
    * @param title 
    * @param data 
    */
-  showNavItemDialog(title,data={}){
+  showNavItemDialog(title, data = {}) {
     this.modal.create({
       nzTitle: title,
       nzContent: FormGroupComponent,
       nzViewContainerRef: this.viewContainerRef,
       nzComponentParams: {
-        params:[
+        params: [
           {
             key: 'id',
             label: 'id',
-            value: data['id']||null,
-            valide:[],
+            value: data['id'] || null,
+            valide: [],
             controlType: 'textbox',
             type: 'hidden',
-          },{
+          }, {
             key: 'title',
             label: '名称',
-            value: data['title']||null,
-            valide:[],
+            value: data['title'] || null,
+            valide: [],
             controlType: 'textbox',
             type: 'text',
-          },{
+          }, {
             key: 'link',
             label: '地址',
-            value: data['link']||null,
-            valide:[],
+            value: data['link'] || null,
+            valide: [],
             controlType: 'textbox',
             type: 'text',
-          },{
+          }, {
             key: 'sort',
             label: '排序',
-            value: data['sort']||null,
-            valide:[],
+            value: data['sort'] || null,
+            valide: [],
             controlType: 'textbox',
             type: 'text',
-          },{
+          }, {
             key: 'navCategoryId',
             label: '分类',
-            value: data['navCategoryId']?data['navCategoryId'].map(v=>v.id):null,
-            valide:[],
+            value: data['navCategoryId'] ? data['navCategoryId'].map(v => v.id) : null,
+            valide: [],
             controlType: 'dropdown',
             type: 'default',
-            options: this.customData.map(v=>({name: v.title, code:v.id}))
+            options: this.customData.map(v => ({ name: v.title, code: v.id }))
           }
         ],
         span: 1,
       },
-      nzOnOk: (component:any) => {
-        this.srv.saveNavItem([component.validateForm.value]).subscribe(v=>{
+      nzOnOk: (component: any) => {
+        this.srv.saveNavItem([component.validateForm.value]).subscribe(v => {
 
         })
       }
     })
-    
+
   }
-  getNavCategory(){
-    this.srv.getNavCategory().subscribe(v=>{
-      if(v.isSuccess()){
+  getNavCategory() {
+    this.srv.getNavCategory().subscribe(v => {
+      if (v.isSuccess()) {
         this.customNavs = this.util.setTree(v.data)
         this.customData = v.data
         this.cf.markForCheck()
       }
     })
   }
-  addNavHtml(str = null){
+  addNavHtml(str = null) {
     // let tem = this.parser.htmlParser(data1.replace(/([\n\r\t]+)/g, ''))
     let data = data1
-    let obj = {}
-    // console.log(this.jsutil.findItem(data, (v)=>v.text == '书签栏'))
-    this.stack=[]
-    this.setItem(data)
-    console.log(this.stack)
-    
+    let a = this.setItem(data, null)
+    console.log(a)
   }
-  stack=[]
-  pos
-  setItem(item){
-    if(this.jsutil.isArray(item)){
-      for(let i=0;i<item.length;i++){
-        // if(item[i].tagName == 'h3' && item[i+1].tagName == 'dl')
-        this.setItem(item[i])
+  setItem(item, nextItem) {
+    if (this.jsutil.isArray(item)) {
+      let arr:any=[]
+      for (let i = 0; i < item.length; i++) {
+        let a
+        if(item[i].tagName == 'h3' && item[i+1].tagName=='dl'){
+          a = this.setItem(item[i], item[i+1])
+          i++
+          
+        }else{
+          a = this.setItem(item[i],null)
+        }
+        if(this.jsutil.isArray(a)){
+          arr.push(...a)
+        }else if(this.jsutil.isObject(a)){
+          arr.push(a)
+        }
       }
-    }else if(this.jsutil.isObject(item)){
-      if(item.children&&item.children.length>0){
-        this.setItem(item.children) 
+      if(arr.length>0){
+        return arr
+      }
+    } else if (this.jsutil.isObject(item)) {
+      let arr: any
+      if (nextItem&&nextItem.children && nextItem.children.length > 0) {
+        arr = this.setItem(nextItem.children,null)
+      }else if(item.children && item.children.length > 0){
+        arr = this.setItem(item.children,null)
       }
       if(item.tagName=='h3'){
         let tem = {
           title: item.text,
-          children: []
+          type: 'sub',
+          children: [...arr]
         }
-        if(!this.pos){
-          this.stack.push(tem)
-        }else{
-          this.pos.children.push(tem)
-        }
-        this.pos=tem
+        return tem
       }
       if(item.tagName=='a'){
+        let link = item.attributes.find(v=>v.name == 'HREF')||{}
         let tem = {
+          type: 'link',
+          link: link.value,
           title: item.text
         }
-        if(this.pos){
-          this.pos.children.push(tem)
-        }
+        return tem
+      }
+      if(arr && arr.length>0){
+        return arr
       }
     }
+    
   }
 }
 let data1 = [{
