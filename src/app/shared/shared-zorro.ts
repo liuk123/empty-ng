231b3361daff1a27a080ed25243877bc3cbb508a
liuk123 <40844000+liuk123@users.noreply.github.com>
2022-05-30 15:11:40 +0800

    1212

--- src/app/shared/shared-zorro.ts
+++ src/app/shared/shared-zorro.ts
@@ -22,7 +22,7 @@ import { NzMessageModule } from 'ng-zorro-antd/message';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 // import { NzNotificationModule } from 'ng-zorro-antd/notification';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-// import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 // import { NzPopoverModule } from 'ng-zorro-antd/popover';
 // import { NzProgressModule } from 'ng-zorro-antd/progress';
 import { NzRadioModule } from 'ng-zorro-antd/radio';
@@ -75,7 +75,7 @@ export const SHARED_ZORRO_MODULES = [
   // NzCardModule,
   // NzDividerModule,
   // NzProgressModule,
-  // NzPopconfirmModule,
+  NzPopconfirmModule,
   // NzNotificationModule,
   NzPaginationModule,
   // NzUploadModule,
