2e022ba55c62a45f0f096783082e06c72daa363b
liuk123 <40844000+liuk123@users.noreply.github.com>
2022-10-13 10:09:02 +0800

    111

--- src/proj/tool/views/demo/demo.component.ts
+++ src/proj/tool/views/demo/demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { HtmlParserService } from 'src/app/core/services/htmlparser.service';
 import { JsUtilService } from 'src/app/shared/utils/js-util';
 import { UtilService } from 'src/app/shared/utils/util';
@@ -10,49 +11,99 @@ import { UtilService } from 'src/app/shared/utils/util';
 })
 export class DemoComponent implements OnInit {
   @ViewChild('inputEl', { read: ElementRef, static: true })
-  inputEl:ElementRef
+  inputEl: ElementRef
 
   inputValue = null
-  conditionValue = null
   resultValue = null
 
-  processData = [
+  processOption = [
     {
-      params: 'a',
-      fn:"return a/2"
+      name: 'filter',
+      paramsType: 'Array',
+      returnType: 'Array',
+      params: ['item', 'index', 'data'],
+      fn: "return item/index",
+      children:[] // 可嵌套循环调取
     },
     {
-      params: 'a',
-      fn:"return a/3"
-    }
+      name: 'map',
+      paramsType: 'Array',
+      returnType: 'Array',
+      params: ['item', 'index', 'data'],
+      fn: "return item/index",
+      children:[]
+    },
+    {
+      name: '正则-array',
+      paramsType: 'string',
+      returnType: 'Array',
+      params: ['item'],
+      fn: "return item/1",
+      condition:{
+        reg: ''
+      },
+      children:[]
+    },
+    {
+      name: '正则-string',
+      paramsType: 'string',
+      returnType: 'string',
+      params: ['item'],
+      fn: "return item/1",
+      condition:{
+        reg: ''
+      },
+      children:[]
+    },
   ]
+
+  validateForm!: FormGroup;
+  get processList() {
+    return this.validateForm.get('processList') as FormArray
+  }
 
   constructor(
     private util: UtilService,
     private htmlPaser: HtmlParserService,
-    private jsUtil: JsUtilService
+    private jsUtil: JsUtilService,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      processList: this.fb.array([])
+    });
   }
-
-  copy(data){
+  // 路由守卫调用
+  isFormDirty() {
+    return this.validateForm.dirty
+  }
+  addItem() {
+    this.processList.push(this.fb.group({
+      name: 'map'
+    }))
+    // this.validateForm.markAsPristine()
+  }
+  removeItem(i){
+    this.processList.removeAt(i)
+  }
+  copy(data) {
     this.util.copyToClipboard(data)
   }
-  uuid(){
+  uuid() {
     console.log(this.util.UUIDGenerator())
   }
 
-  parser(){
-    let htmlstr=``
+  parser() {
+    let htmlstr = ``
     let i = htmlstr.indexOf('<body>')
     let lasti = htmlstr.lastIndexOf('</body>')
     if (i > 0) {
       htmlstr = htmlstr.slice(i, lasti + 7)
     }
     let obj = this.htmlPaser.htmlParser(htmlstr)
-    this.jsUtil.findItem(obj,v=>{
-      if(v.attributes.some(val=>val.value=='HotItem-content')){
+    this.jsUtil.findItem(obj, v => {
+      if (v.attributes.some(val => val.value == 'HotItem-content')) {
         console.log(v)
       }
     })
@@ -64,14 +115,14 @@ export class DemoComponent implements OnInit {
    * 正则查找--替换--map--filter--find
    * tree--findItem 
    */
-  processMap(v){
-    let result=v
-    this.processData.forEach(item=>{
-      result = new Function(item.params, item.fn)(result)
+  processMap(v) {
+    let result = v
+    this.processOption.forEach(item => {
+      result = new Function(item.params.join(','), item.fn)(result)
     })
     return result
   }
-  regRex(){
+  regRex() {
     let ret = this.processMap(this.inputValue)
     console.log(ret)
   }
