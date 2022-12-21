59e71dfdf8f69e8a8b1b26163eff9ee9b846cba3
liuk123 <40844000+liuk123@users.noreply.github.com>
2022-12-21 13:01:02 +0800

    312312321

--- src/proj/tool/views/demo/demo.component.ts
+++ src/proj/tool/views/demo/demo.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { HtmlParserService } from 'src/app/core/services/htmlparser.service';
+import { MessageUtilService } from 'src/app/core/services/message-util.service';
 import { JsUtilService } from 'src/app/shared/utils/js-util';
 import { UtilService } from 'src/app/shared/utils/util';
 
@@ -18,12 +19,34 @@ export class DemoComponent implements OnInit {
 
   processOption = [
     {
+      name: 'JSON.parse',
+      paramsType: 'String',
+      returnType: 'Array',
+      params: ['data'],
+      fn: `return JSON.parse(data)`,
+      inputStr: null,
+      fnBody: null,
+      desc: 'string转数组对象'
+    },
+    {
+      name: 'JSON.stringify',
+      paramsType: 'Array',
+      returnType: 'String',
+      params: ['data'],
+      fn: `return JSON.stringify(data)`,
+      inputStr: null,
+      fnBody: null,
+      desc: '数组对象转string'
+    },
+    {
       name: 'filter',
       paramsType: 'Array',
       returnType: 'Array',
-      params: ['item', 'index', 'data'],
-      fn: "return item/index",
-      children:[] // 可嵌套循环调取
+      params: ['data', 'inputStr'],
+      fn: `return data.filter(v=>fnBody)`,
+      inputStr: null,
+      fnBody: 'Number(v[0])>10',
+      desc: '请输入filter的函数体'
     },
     {
       name: 'map',
@@ -36,8 +59,8 @@ export class DemoComponent implements OnInit {
       desc: '请输入map的函数体'
     },
     {
-      name: '正则-array',
-      paramsType: 'string',
+      name: '正则return array',
+      paramsType: 'String',
       returnType: 'Array',
       params: ['item','regStr'],
       fn: `const reg = new RegExp(regStr,'g')
@@ -62,6 +85,7 @@ export class DemoComponent implements OnInit {
     private htmlPaser: HtmlParserService,
     private jsUtil: JsUtilService,
     private fb: FormBuilder,
+    private messageSrv: MessageUtilService
   ) { }
 
   ngOnInit(): void {
@@ -113,17 +137,19 @@ export class DemoComponent implements OnInit {
   /**
    * 流程
    * 正则查找--替换--map--filter--find
-   * tree--findItem 
    */
-  processMap(v,i) {
+  processMap(v, i, paramsType) {
     let ret = null, tem = null;
     let item = this.processList.value[i]
+    if(item.value.paramsType!=paramsType){
+      this.messageSrv.error(`${item.value.name}接收数据格式错误`)
+    }
     tem = new Function(
       item.value.params.join(','),
       item.fnBody?item.value.fn.replace('fnBody', item.fnBody):item.value.fn)(v, item.inputStr)
     console.log(item.value.name,tem)
     if(i<this.processList.length-1){
-      ret = this.processMap(tem,++i)
+      ret = this.processMap(tem, ++i, item.value.returnType)
       return ret
     }else{
       return tem
@@ -131,7 +157,7 @@ export class DemoComponent implements OnInit {
     
   }
   run() {
-    let ret = this.processMap(this.inputValue, 0)
+    let ret = this.processMap(this.inputValue, 0, 'String')
     console.log('结果：',ret)
     this.resultValue = ret
   }
