a236ffeb939dd6f69f15590585870b0dea39a172
liuk123 <40844000+liuk123@users.noreply.github.com>
2022-12-21 12:34:24 +0800

    1222

--- src/proj/tool/views/demo/demo.component.ts
+++ src/proj/tool/views/demo/demo.component.ts
@@ -29,32 +29,27 @@ export class DemoComponent implements OnInit {
       name: 'map',
       paramsType: 'Array',
       returnType: 'Array',
-      params: ['item', 'index', 'data'],
-      fn: "return item/index",
-      children:[]
+      params: ['data', 'inputStr'],
+      fn: `return data.map(v=>fnBody)`,
+      inputStr: null,
+      fnBody: 'v[0]',
+      desc: '请输入map的函数体'
     },
     {
       name: '正则-array',
       paramsType: 'string',
       returnType: 'Array',
-      params: ['item'],
-      fn: "return item/1",
-      condition:{
-        reg: ''
-      },
-      children:[]
-    },
-    {
-      name: '正则-string',
-      paramsType: 'string',
-      returnType: 'string',
-      params: ['item'],
-      fn: "return item/1",
-      condition:{
-        reg: ''
-      },
-      children:[]
-    },
+      params: ['item','regStr'],
+      fn: `const reg = new RegExp(regStr,'g')
+        const arr=[]
+        let temArr=null
+        while((temArr = reg.exec(item))!==null){
+          arr.push(temArr)
+        }
+        return arr`,
+      inputStr: '[0-9]+',
+      desc: '请输入正则表达式'
+    }
   ]
 
   validateForm!: FormGroup;
@@ -74,15 +69,20 @@ export class DemoComponent implements OnInit {
       processList: this.fb.array([])
     });
   }
+  setInputValue(i){
+    (this.processList.controls[i] as FormGroup).controls.inputStr.setValue(this.processList.value[i].value.inputStr);
+    (this.processList.controls[i] as FormGroup).controls.fnBody.setValue(this.processList.value[i].value.fnBody)
+  }
   // 路由守卫调用
   isFormDirty() {
     return this.validateForm.dirty
   }
   addItem() {
     this.processList.push(this.fb.group({
-      name: 'map'
+      value: null,
+      inputStr: null,
+      fnBody: null,
     }))
-    // this.validateForm.markAsPristine()
   }
   removeItem(i){
     this.processList.removeAt(i)
@@ -115,15 +115,24 @@ export class DemoComponent implements OnInit {
    * 正则查找--替换--map--filter--find
    * tree--findItem 
    */
-  processMap(v) {
-    let result = v
-    this.processOption.forEach(item => {
-      result = new Function(item.params.join(','), item.fn)(result)
-    })
-    return result
+  processMap(v,i) {
+    let ret = null, tem = null;
+    let item = this.processList.value[i]
+    tem = new Function(
+      item.value.params.join(','),
+      item.fnBody?item.value.fn.replace('fnBody', item.fnBody):item.value.fn)(v, item.inputStr)
+    console.log(item.value.name,tem)
+    if(i<this.processList.length-1){
+      ret = this.processMap(tem,++i)
+      return ret
+    }else{
+      return tem
+    }
+    
   }
-  regRex() {
-    let ret = this.processMap(this.inputValue)
-    console.log(ret)
+  run() {
+    let ret = this.processMap(this.inputValue, 0)
+    console.log('结果：',ret)
+    this.resultValue = ret
   }
 }
