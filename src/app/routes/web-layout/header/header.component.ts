55655e613bd002bcf20a2eea6c1c077df0412606
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-03-10 10:55:47 +0800

    333

--- src/app/routes/web-layout/header/header.component.ts
+++ src/app/routes/web-layout/header/header.component.ts
@@ -1,15 +1,112 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzDrawerService } from 'ng-zorro-antd/drawer';
+import { fromEvent, Subject } from 'rxjs';
+import { debounceTime, map, mergeMap, takeUntil } from 'rxjs/operators';
+import { Menu } from 'src/app/biz/model/common/menu.model';
+import { User } from 'src/app/biz/model/common/user.model';
+import { MenuService } from 'src/app/biz/services/common/menu.service';
+import { UserService } from 'src/app/biz/services/common/user.service';
 import { ConfigService } from 'src/app/core/services/config.service';
+import { AppReuseStrategy } from 'src/app/core/services/route-reuse';
+import { MenuTreeComponent } from 'src/app/shared/components/menu-tree/menu-tree.component';
+import { JsUtilService } from 'src/app/shared/utils/js-util';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.less']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   
   title=ConfigService.Config.systemName
-  constructor() { }
 
-  ngOnInit(): void {}
+  unsub$ = new Subject()
+  menus: Menu[]
+  userInfo: User
+  history = null
+
+  constructor(
+    private menuSrv: MenuService,
+    private userSrv: UserService,
+    private router: Router,
+    private drawerService: NzDrawerService,
+    private jsutil: JsUtilService,
+  ) { 
+
+  }
+
+  ngOnInit(): void {
+    this.menuSrv.menuEvent.pipe(takeUntil(this.unsub$)).subscribe(v => {
+      this.menus = v
+    })
+    this.userSrv.userEvent.pipe(takeUntil(this.unsub$)).subscribe(v => {
+      this.userInfo = v
+    });
+    this.menuSrv.historyEvent.pipe(takeUntil(this.unsub$)).subscribe(v => {
+      if (v?.length > 0) {
+        this.history = v
+      }
+    }) 
+
+  }
+  ngOnDestroy(): void {
+    this.unsub$.next()
+    this.unsub$.complete()
+  }
+
+  logout() {
+    this.userSrv.logout().subscribe(res => {
+      if (res.isSuccess()) {
+        this.userSrv.reLoadUserInfo(null)
+        this.menuSrv.loadNoUserMenuData().subscribe(res => {
+          this.menuSrv.setMenus(res)
+          this.router.navigate(['./blog/home'])
+        })
+
+      }
+    });
+  }
+  openMenuDrawer() {
+    const cloneData = this.jsutil.clone(this.menus, (item) => {
+      if (item.isMenuShow !== false) {
+        return item
+      }
+    })
+    const drawerRef = this.drawerService.create({
+      nzTitle: 'cicode menu',
+      nzContent: MenuTreeComponent,
+      nzContentParams: {
+        data: cloneData,
+      }
+    })
+    drawerRef.afterOpen.pipe(
+      mergeMap(() => drawerRef.getContentComponent().ckEvent),
+      takeUntil(this.unsub$)
+    ).subscribe(v => {
+      if (v.type === 'router') {
+        this.router.navigate([v.route])
+        drawerRef.close()
+      } else if (v.type === 'link') {
+        window.open(v.link, '_blank')
+        drawerRef.close()
+      }
+    })
+  }
+  fullScrean(){
+    if (!document.fullscreenElement) {
+      //进入页面全屏
+      document.documentElement.requestFullscreen();
+    } else {
+      if (document.exitFullscreen) {
+        //退出全屏
+        document.exitFullscreen();
+      }
+    }
+  }
+  delHistoryItem(e, i, data) {
+    e.preventDefault()
+    this.menuSrv.delHistoryItem(i)
+    AppReuseStrategy.delRoute(data.route)
+  }
 }
--- src/app/routes/web-layout/web-layout.component.ts
+++ src/app/routes/web-layout/web-layout.component.ts
@@ -1,17 +1,9 @@
 import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { NzDrawerService } from 'ng-zorro-antd/drawer';
-import { fromEvent, Subject } from 'rxjs';
-import { debounceTime, map, mergeMap, takeUntil } from 'rxjs/operators';
-import { Menu, BreadcrumbMenu } from 'src/app/biz/model/common/menu.model';
-import { User } from 'src/app/biz/model/common/user.model';
-import { ConfigService } from 'src/app/core/services/config.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { BreadcrumbMenu } from 'src/app/biz/model/common/menu.model';
 import { MenuService } from 'src/app/biz/services/common/menu.service';
-import { UserService } from 'src/app/biz/services/common/user.service';
-import { AppReuseStrategy } from 'src/app/core/services/route-reuse';
-import { MenuTreeComponent } from 'src/app/shared/components/menu-tree/menu-tree.component';
-import { JsUtilService } from 'src/app/shared/utils/js-util';
 
 export const slideInAnimation =
   trigger('routeAnimations', [
@@ -51,23 +43,12 @@ export const slideInAnimation =
 })
 export class WebLayoutComponent implements OnInit, OnDestroy {
 
-  menus: Menu[]
+  
   breadcrumbMenus: BreadcrumbMenu[] = [];
-  history = null
-  userInfo: User;
-  isMobile: Boolean
   unsub$ = new Subject()
-
-  $httpLoading
-  // routeAnimation = null
   constructor(
     private menuSrv: MenuService,
-    private userSrv: UserService,
-    private router: Router,
-    private drawerService: NzDrawerService,
-    private jsutil: JsUtilService,
   ) {
-    this.isMobile = this.isMobileFn()
     /**
      * 面包屑菜单
      */
@@ -86,103 +67,16 @@ export class WebLayoutComponent implements OnInit, OnDestroy {
         this.breadcrumbMenus = v
       }
     })
-    this.menuSrv.historyEvent.pipe(takeUntil(this.unsub$)).subscribe(v => {
-      if (v?.length > 0) {
-        this.history = v
-      }
-    })
-    // this.userSrv.getCurrentUser().subscribe(v => {
-    //   if (v?.data) {
-    //     this.userSrv.reLoadUserInfo(v.data)
-    //   }
-    // })
-    this.menuSrv.menuEvent.pipe(takeUntil(this.unsub$)).subscribe(v => {
-      this.menus = v
-    })
-    this.userSrv.userEvent.pipe(takeUntil(this.unsub$)).subscribe(v => {
-      this.userInfo = v
-    });
-
-    // let stableRef = this.appRef.isStable.pipe(first(isStable => isStable === true))
-
-    if (ConfigService.Config.isBrowser) {
-
-      fromEvent(window, 'resize').pipe(
-        debounceTime(100),
-        map(() => this.isMobileFn()),
-        takeUntil(this.unsub$)
-      ).subscribe(v => {
-        if (this.isMobile !== v) {
-          this.isMobile = v
-        }
-      })
-    }
   }
   ngOnDestroy() {
     this.unsub$.next()
     this.unsub$.complete()
   }
-  isMobileFn() {
-    return ConfigService.Config.isBrowser && window.screen.availWidth < 720
-  }
-  delHistoryItem(e, i, data) {
-    e.preventDefault()
-    this.menuSrv.delHistoryItem(i)
-    AppReuseStrategy.delRoute(data.route)
-  }
+
   toTop() {
     window.scroll({
       top: 0,
       behavior: 'smooth'
-    })
-  }
-  fullScrean(){
-    if (!document.fullscreenElement) {
-      //进入页面全屏
-      document.documentElement.requestFullscreen();
-    } else {
-      if (document.exitFullscreen) {
-        //退出全屏
-        document.exitFullscreen();
-      }
-    }
-  }
-  logout() {
-    this.userSrv.logout().subscribe(res => {
-      if (res.isSuccess()) {
-        this.userSrv.reLoadUserInfo(null)
-        this.menuSrv.loadNoUserMenuData().subscribe(res => {
-          this.menuSrv.setMenus(res)
-          this.router.navigate(['./blog/home'])
-        })
-
-      }
-    });
-  }
-  openMenuDrawer() {
-    const cloneData = this.jsutil.clone(this.menus, (item) => {
-      if (item.isMenuShow !== false) {
-        return item
-      }
-    })
-    const drawerRef = this.drawerService.create({
-      nzTitle: 'cicode menu',
-      nzContent: MenuTreeComponent,
-      nzContentParams: {
-        data: cloneData,
-      }
-    })
-    drawerRef.afterOpen.pipe(
-      mergeMap(() => drawerRef.getContentComponent().ckEvent),
-      takeUntil(this.unsub$)
-    ).subscribe(v => {
-      if (v.type === 'router') {
-        this.router.navigate([v.route])
-        drawerRef.close()
-      } else if (v.type === 'link') {
-        window.open(v.link, '_blank')
-        drawerRef.close()
-      }
     })
   }
 }
