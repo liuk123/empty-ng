8e75e622f9a2986fe13c76d98b80ddf29af90274
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-03-29 12:07:26 +0800

    33

--- src/proj/dynamic/views/chart-comp/line-chart/line-chart.component.ts
+++ src/proj/dynamic/views/chart-comp/line-chart/line-chart.component.ts
@@ -6,14 +6,14 @@ import { ChartType } from 'src/app/shared/directive/g2chart.directive';
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.less']
 })
-export class LineChartComponent implements OnInit, OnChanges {
+export class LineChartComponent implements OnInit {
 
   @Input() data=null;
   @Input() type: ChartType = 'interval'
   constructor() { }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log("changes",changes)
-  }
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log("changes",changes)
+  // }
 
   ngOnInit(): void {
   }
