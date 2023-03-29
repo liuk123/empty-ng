ac48ced70aacf9fa374bffc9057cc818bfa5342e
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-03-29 12:06:44 +0800

    333

--- src/app/shared/directive/permissions.directive.ts
+++ src/app/shared/directive/permissions.directive.ts
@@ -10,14 +10,15 @@ export class PermissionsDirective implements OnInit, OnDestroy {
   @Input() permissions = null
   unsub:Unsubscribable
   constructor(private el: ElementRef,private userSvr: UserService, private rd: Renderer2) {
-  }
-
-  ngOnInit():void {
     this.unsub = this.userSvr.userEvent.subscribe(u=>{
       if(!this.permissions || !u || !u.authorities.some(v=> v.name === this.permissions)){
         this.rd.removeChild(this.el.nativeElement.parentNode, this.el.nativeElement)
       }
     })
+  }
+
+  ngOnInit():void {
+    
   }
   ngOnDestroy():void {
     this.unsub.unsubscribe()
