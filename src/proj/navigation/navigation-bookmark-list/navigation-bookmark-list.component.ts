b9085a3877cbceb9f238f4703fcb93cb324dc67e
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-04-21 11:20:30 +0800

    222

--- src/proj/navigation/navigation-bookmark-list/navigation-bookmark-list.component.ts
+++ src/proj/navigation/navigation-bookmark-list/navigation-bookmark-list.component.ts
@@ -1,13 +1,10 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewContainerRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { NavigationService } from '../service/navigation.service';
 import { Navigation } from '../model/navigation';
 import { ConfigService } from 'src/app/core/services/config.service';
-import { NzModalService } from 'ng-zorro-antd/modal';
-import { MessageUtilService } from 'src/app/core/services/message-util.service';
-import { FormGroupComponent } from 'src/app/shared/components/form-group/form-group.component';
 import { JsUtilService } from 'src/app/shared/utils/js-util';
 import { MenuService } from 'src/app/biz/services/common/menu.service';
 
@@ -21,9 +18,8 @@ export class NavigationBookmarkListComponent implements OnInit {
   unsub$ = new Subject()
   id:string
   navigation: Navigation
-  categoryData
+  // categoryData
   categoryTree
-  isEdit = false
   faviconUrl = ConfigService.Config.faviconUrl
   defaultFavicon = 'assets/image/common/nofavicon.svg'
 
@@ -35,9 +31,6 @@ export class NavigationBookmarkListComponent implements OnInit {
     private srv: NavigationService,
     private cf: ChangeDetectorRef,
     private jsUtil: JsUtilService,
-    private modal: NzModalService,
-    private viewContainerRef: ViewContainerRef,
-    private message: MessageUtilService,
     private menuSrv: MenuService,
     ) { }
 
@@ -62,188 +55,7 @@ export class NavigationBookmarkListComponent implements OnInit {
       }
     })
   }
-  delBookmarkItem(id, pid) {
-    this.srv.delBookmarkItem(id).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        this.getBookmarkCategoryById()
-      }
-    })
-  }
-  delNavCategory(id) {
-    this.srv.delBookmarkCategory(id).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        this.getBookmarkCategory(true)
-      }
-    })
-  }
-
-  /**
-   * 导航分类添加编辑
-   * @param title 
-   * @param data 
-   */
-  showCategoryDialog(title, data = {}) {
-    let params = [
-      {
-        key: 'id',
-        label: 'id',
-        value: data['id'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'hidden',
-      }, {
-        key: 'title',
-        label: '名称',
-        value: data['title'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'text',
-      }, {
-        key: 'icon',
-        label: '图标',
-        value: data['icon'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'text',
-      }, {
-        key: 'sort',
-        label: '排序',
-        value: data['sort'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'text',
-      }, {
-        key: 'pid',
-        label: '父级',
-        value: data['pid'] ? data['pid'] : null,
-        valide: [],
-        controlType: 'dropdown',
-        type: 'default',
-        options: this.categoryData.filter(v => v.pid === null).map(v => ({ name: v.title, code: v.id }))
-      }
-    ]
-    this.modal.create({
-      nzTitle: title,
-      nzContent: FormGroupComponent,
-      nzViewContainerRef: this.viewContainerRef,
-      nzMaskClosable: false,
-      nzComponentParams: {
-        params: params,
-        span: 1,
-      },
-      nzOnOk: (component: any) => {
-        this.saveBookmarkCategory(component.validateForm.value)
-      }
-    })
-  }
-  /**
-   * 导航添加编辑
-   * @param title 
-   * @param data 
-   */
-  showItemDialog(title, data = {}, pdata=null) {
-    this.modal.create({
-      nzTitle: title,
-      nzContent: FormGroupComponent,
-      nzViewContainerRef: this.viewContainerRef,
-      nzMaskClosable: false,
-      nzComponentParams: {
-        params: [
-          {
-            key: 'id',
-            label: 'id',
-            value: data['id'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'hidden',
-          }, {
-            key: 'title',
-            label: '名称',
-            value: data['title'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'link',
-            label: '地址',
-            value: data['link'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'descItem',
-            label: '描述',
-            value: data['descItem'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'icon',
-            label: '图标',
-            value: data['icon'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'categoryId',
-            label: '分类',
-            value: pdata?.['id'],
-            valide: [],
-            controlType: 'dropdown',
-            type: 'default',
-            options: this.categoryData.map(v => ({ name: v.title, code: v.id }))
-          }
-        ],
-        span: 1,
-      },
-      nzOnOk: (component: any) => {
-        const value = component.validateForm.value
-        if(!pdata){
-          let tem = this.categoryData.find(v=>v.id == value.categoryId)
-          pdata = {pid: tem.pid??tem.id}
-        }
-        this.srv.saveFavicon({url: value.link}).subscribe(d=>{
-          if(d.isSuccess()){
-            this.message.info(d.resultMsg)
-            if(d.data!==null){
-              value.icon = d.data
-            }
-          }
-          this.saveBookmarkItem(value, pdata)
-        })
-        
-      }
-    })
-
-  }
-
-  /**
-   * 保存书签
-   * @param data 
-   */
-  saveBookmarkItem(data, pdata) {
-    this.srv.saveBookmarkItem(data).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        
-        this.getBookmarkCategoryById()
-      }
-    })
-  }
-  /**
-   * 保存修改书签分类
-   * @param data 
-   */
-  saveBookmarkCategory(data) {
-    this.srv.saveBookmarkCategory(data).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        this.getBookmarkCategory(true)
-      }
-    })
-  }
+  
   selectNav(e){
     e.selected=!e.selected
   }
@@ -252,13 +64,13 @@ export class NavigationBookmarkListComponent implements OnInit {
     this.srv.getBookmarkCategory(isDelStateKey).subscribe(res => {
       if (res.isSuccess()) {
         let selItem = res.data.find(v=>v.id == this.id)
-        this.categoryData = res.data.map(v => ({
+        let tem = res.data.map(v => ({
           ...v,
           selected: selItem.pid == v.id,
           type: v.pid==null?'sub':'router',
           route: '/nav/list/'+v.id
         }))
-        this.categoryTree = this.jsUtil.setTree(this.categoryData)
+        this.categoryTree = this.jsUtil.setTree(tem)
         this.cf.markForCheck()
       }
     })
--- src/proj/navigation/navigation-bookmark/navigation-bookmark.component.ts
+++ src/proj/navigation/navigation-bookmark/navigation-bookmark.component.ts
@@ -1,9 +1,7 @@
-import { ApplicationRef, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewContainerRef } from '@angular/core';
-import { NzModalService } from 'ng-zorro-antd/modal';
+import { ApplicationRef, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit } from '@angular/core';
 import { Observable, zip } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { MessageUtilService } from 'src/app/core/services/message-util.service';
-import { FormGroupComponent } from 'src/app/shared/components/form-group/form-group.component';
 import { Navigation } from '../model/navigation';
 import { NavigationService } from '../service/navigation.service';
 import { ConfigService } from 'src/app/core/services/config.service';
@@ -20,7 +18,6 @@ export class NavigationBookmarkComponent implements OnInit {
 
   faviconUrl = ConfigService.Config.faviconUrl
   defaultFavicon = 'assets/image/common/nofavicon.svg'
-  categoryData = []
   categoryTree = []
   selData: Navigation[]
 
@@ -33,15 +30,12 @@ export class NavigationBookmarkComponent implements OnInit {
   get historyNavList(){
     return this.lastNavData.slice(0,15)
   }
-  isEdit = false
 
   constructor(
     private srv: NavigationService,
     private jsUtil: JsUtilService,
     private cf: ChangeDetectorRef,
     private el: ElementRef,
-    private modal: NzModalService,
-    private viewContainerRef: ViewContainerRef,
     private message: MessageUtilService,
     private appRef: ApplicationRef,
   ) { }
@@ -105,12 +99,12 @@ export class NavigationBookmarkComponent implements OnInit {
   getBookmarkCategory(isDelStateKey = false) {
     this.srv.getBookmarkCategory(isDelStateKey).subscribe(res => {
       if (res.isSuccess()) {
-        this.categoryData = res.data.map(v => ({
+        let tem = res.data.map(v => ({
           ...v,
           slugger: 'h_'+this.slugger.slug(v.title, { dryrun: true }),
           selected: false
         }))
-        this.categoryTree = this.jsUtil.setTree(this.categoryData)
+        this.categoryTree = this.jsUtil.setTree(tem)
         if (ConfigService.Config.isBrowser) {
           let bookmarkId = window.localStorage.getItem('bookmarkId')
           if (bookmarkId) {
@@ -128,200 +122,7 @@ export class NavigationBookmarkComponent implements OnInit {
   getBookmarkCategoryByPid(id, isDelStateKey = false): Observable<any> {
     return this.srv.getBookmarkCategoryByPid({id, size:21}, isDelStateKey)
   }
-  delBookmarkItem(id, pid) {
-    this.srv.delBookmarkItem(id).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        this.getBookmarkCategoryByPid(pid, true).subscribe(res => {
-          this.selData = res.data.map(v=>({
-            ...v,
-            slugger: 'h_'+this.slugger.slug(v.title, { dryrun: true }),
-          }))
-          this.cf.markForCheck()
-        })
-      }
-    })
-  }
-  delNavCategory(id) {
-    this.srv.delBookmarkCategory(id).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        this.getBookmarkCategory(true)
-      }
-    })
-  }
-  /**
-   * 导航分类添加编辑
-   * @param title 
-   * @param data 
-   */
-  showCategoryDialog(title, data = {}) {
-    let params = [
-      {
-        key: 'id',
-        label: 'id',
-        value: data['id'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'hidden',
-      }, {
-        key: 'title',
-        label: '名称',
-        value: data['title'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'text',
-      }, {
-        key: 'icon',
-        label: '图标',
-        value: data['icon'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'text',
-      }, {
-        key: 'sort',
-        label: '排序',
-        value: data['sort'] || null,
-        valide: [],
-        controlType: 'textbox',
-        type: 'text',
-      }, {
-        key: 'pid',
-        label: '父级',
-        value: data['pid'] ? data['pid'] : null,
-        valide: [],
-        controlType: 'dropdown',
-        type: 'default',
-        options: this.categoryData.filter(v => v.pid === null).map(v => ({ name: v.title, code: v.id }))
-      }
-    ]
-    this.modal.create({
-      nzTitle: title,
-      nzContent: FormGroupComponent,
-      nzViewContainerRef: this.viewContainerRef,
-      nzMaskClosable: false,
-      nzComponentParams: {
-        params: params,
-        span: 1,
-      },
-      nzOnOk: (component: any) => {
-        this.saveBookmarkCategory(component.validateForm.value)
-      }
-    })
-  }
-  /**
-   * 导航添加编辑
-   * @param title 
-   * @param data 
-   */
-  showItemDialog(title, data = {}, pdata=null) {
-    this.modal.create({
-      nzTitle: title,
-      nzContent: FormGroupComponent,
-      nzViewContainerRef: this.viewContainerRef,
-      nzMaskClosable: false,
-      nzComponentParams: {
-        params: [
-          {
-            key: 'id',
-            label: 'id',
-            value: data['id'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'hidden',
-          }, {
-            key: 'title',
-            label: '名称',
-            value: data['title'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'link',
-            label: '地址',
-            value: data['link'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'descItem',
-            label: '描述',
-            value: data['descItem'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'icon',
-            label: '图标',
-            value: data['icon'] || null,
-            valide: [],
-            controlType: 'textbox',
-            type: 'text',
-          }, {
-            key: 'categoryId',
-            label: '分类',
-            value: pdata?.['id'],
-            valide: [],
-            controlType: 'dropdown',
-            type: 'default',
-            options: this.categoryData.map(v => ({ name: v.title, code: v.id }))
-          }
-        ],
-        span: 1,
-      },
-      nzOnOk: (component: any) => {
-        const value = component.validateForm.value
-        if(!pdata){
-          let tem = this.categoryData.find(v=>v.id == value.categoryId)
-          pdata = {pid: tem.pid??tem.id}
-        }
-        this.srv.saveFavicon({url: value.link}).subscribe(d=>{
-          if(d.isSuccess()){
-            this.message.info(d.resultMsg)
-            if(d.data!==null){
-              value.icon = d.data
-            }
-          }
-          this.saveBookmarkItem(value, pdata)
-        })
-        
-      }
-    })
-
-  }
-
-  /**
-   * 保存书签
-   * @param data 
-   */
-  saveBookmarkItem(data, pdata) {
-    this.srv.saveBookmarkItem(data).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        
-        this.getBookmarkCategoryByPid(pdata.pid, true).subscribe(res => {
-          this.selData = res.data.map(v=>({
-            ...v,
-            slugger: 'h_'+this.slugger.slug(v.title, { dryrun: true }),
-          }))
-          this.cf.markForCheck()
-        })
-      }
-    })
-  }
-  /**
-   * 保存修改书签分类
-   * @param data 
-   */
-  saveBookmarkCategory(data) {
-    this.srv.saveBookmarkCategory(data).subscribe(res => {
-      if (res.isSuccess()) {
-        this.message.info(res.resultMsg)
-        this.getBookmarkCategory(true)
-      }
-    })
-  }
-
+  
   goPage(item){
     let i = this.lastNavData.findIndex(v=>v.id == item.id)
     if(i!==-1){
