97dbeabefb906b9c264d753108f4ff30110c902b
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-05-30 14:02:24 +0800

    2

--- src/app/routes/web-layout/footer/footer.component.ts
+++ src/app/routes/web-layout/footer/footer.component.ts
@@ -8,9 +8,15 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  sub=null
   constructor() { }
 
   ngOnInit(): void {
+    let t1 = new Date().getTime() - new Date('2022-02-07').getTime()
+    let y = Math.floor(t1/1000/60/60/24/365)
+    let m = Math.floor(t1/1000/60/60/24/30%12)
+    let d = Math.floor(t1/1000/60/60/24%30)
+    this.sub = `${y}年${m}月${d}天`
   }
   emailto(e){
     e.preventDefault()
