b870679fe9f7eafb64fedf2644498e7a1ae83dc5
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-06-01 09:06:47 +0800

    3

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,20 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { platformFactory } from './app/core/services/config.service';
 import { environment } from './environments/environment';
+// import { NzConfig } from 'ng-zorro-antd/core/config';
 
 if (environment.production) {
   enableProdMode();
 }
+// const ngZorroConfig: NzConfig = {
+//   // 注意组件名称没有 nz 前缀
+//   theme: {
+//     primaryColor: '#007ACC',
+//     accentColor: '#D108FA',
+//     bodyBackground: '#fafafa',
+//     backgroundColor: 'rgba(0,0,0,.03)'
+//   }
+// };
 
 document.addEventListener('DOMContentLoaded', () => {
   let fontSize = Math.floor(window.screen.width/1440 * 15 * 100 + 0.5)/100
@@ -18,7 +28,10 @@ document.addEventListener('DOMContentLoaded', () => {
       provide: PLATFORM_INITIALIZER,
       useFactory: platformFactory,
       multi: true,
-    }
+    },
+    // {
+    //   provide: NZ_CONFIG, useValue: ngZorroConfig
+    // }
   ]).bootstrapModule(AppModule)
     .then((ref)=>{
       if(window['ngRef']){
