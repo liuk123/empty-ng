b5ddd1cff53ff577b5611fe40a6ef3786f3fb199
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-07-12 09:59:26 +0800

    333

--- src/proj/blog/blog-home/blog-home.component.ts
+++ src/proj/blog/blog-home/blog-home.component.ts
@@ -2,8 +2,9 @@ import { ApplicationRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } f
 import { Subject } from 'rxjs';
 import { debounceTime, first } from 'rxjs/operators';
 import { PageInfo } from 'src/app/biz/model/common/page-info.model';
-import { ArtItem, ArticleType } from '../model/artlist.model';
+import { ArtItem } from '../model/artlist.model';
 import { ArticleService } from '../services/article.service';
+import { JsUtilService } from 'src/app/shared/utils/js-util';
 
 @Component({
   selector: 'app-blog-home',
@@ -12,17 +13,17 @@ import { ArticleService } from '../services/article.service';
 })
 export class BlogHomeComponent implements OnInit, OnDestroy {
   @ViewChild('artAnchor', {read: ElementRef}) anchor: ElementRef
-  listData:ArtItem[]
-  tagData = []
   tagColunm = []
-  tagIndex = null
-  recommend
+  selTag=[]
+  tagItem = null
+  recommend =null
   sel$ = new Subject()
-
   listPageData: PageInfo<ArtItem>= new PageInfo([],1,10);
+
   constructor(
     private articleSrv: ArticleService,
     private appRef: ApplicationRef,
+    private jsUtilSrv: JsUtilService
   ) {}
 
   ngOnInit(): void {
@@ -41,16 +42,6 @@ export class BlogHomeComponent implements OnInit, OnDestroy {
     this.articleSrv.getTagColumn().subscribe((tagRes)=>{
       if(tagRes.isSuccess()){
         this.tagColunm = tagRes.data
-        if(this.tagIndex!==null){
-          this.tagData = this.tagColunm[this.tagIndex]?.tagList
-        }else{
-          this.tagData = []
-          this.tagColunm.forEach(val=>{
-            val.tagList.forEach(v=>{
-              this.tagData.push(v)
-            })
-          })
-        }
         this.load(1,false)
         this.getRecommendArticle()
       }
@@ -60,8 +51,8 @@ export class BlogHomeComponent implements OnInit, OnDestroy {
     let params={
       pageIndex: n,
       pageSize: this.listPageData.pageSize,
-      tagIds: this.tagData.filter(v=>v.isSelected).map(v=>v.id),
-      tagColumnId: this.tagIndex!==null?this.tagColunm[this.tagIndex]?.id:'',
+      tagIds: this.selTag.map(v=>v.id),
+      tagColumnId: this.tagItem?.id
     }
     this.articleSrv.getArticles(params).subscribe(res=>{
       if(res.isSuccess()){
@@ -73,8 +64,6 @@ export class BlogHomeComponent implements OnInit, OnDestroy {
         if(isAnthor){
           this.scrollInto(this.anchor)
         }
-        // this.anchor.nativeElement.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
-        // window.scrollTo({top:0})
       }
     })
   }
@@ -83,37 +72,65 @@ export class BlogHomeComponent implements OnInit, OnDestroy {
       el.nativeElement.scrollIntoView({ block: 'start', inline: 'nearest', behavior: 'smooth' });
     })
   }
-  switchTitle(i=null){
 
-    this.tagIndex=i
-    if(this.tagIndex!==null){
-      this.tagData = this.tagColunm[this.tagIndex]?.tagList.map(v=>({
-        ...v,
-        isSelected: false,
-      }))
-    }else{
-      this.tagData = []
-      this.tagColunm.forEach(val=>{
-        val.tagList.forEach(v=>{
-          this.tagData.push({
-            ...v,
-            isSelected: false
-          })
-        })
-      })
-    }
-    
-    this.sel$.next()
-    this.getRecommendArticle()
-  }
-  selectEvent(data){
-    data.isSelected=!data.isSelected
-    this.sel$.next()
-  }
   getRecommendArticle(){
     this.articleSrv.getLink().subscribe(res=>{
-      this.recommend = res.data.filter(v=>this.tagIndex==null || !v.category || v.category==this.tagColunm[this.tagIndex]?.id)
+      this.recommend = res.data.filter(v=>!this.tagItem || v?.category==this.tagItem.id)
     })
   }
+  selectTag(data){
+    if('tagList' in data){
+      data.selected = true
+      this.tagColunm.forEach(v=>{
+        if(v.id!=data.id){
+          v.selected = false 
+        }
+      })
+      if(data.active){
+        data.active = false
+      }else{
+        data.active = true
+        this.jsUtilSrv.loopTree(this.tagColunm, (v)=>{
+          if(v.title != data.title||v.id!=data.id){
+            v.active = false
+          }
+        }, {mapObject:['tagList']})
+      }
+      this.tagItem = data
+      this.selTag = []
+    }else{
+      if(data.active){
+        data.active = false
+        this.selTag.splice(this.selTag.findIndex(v=>v.id == data.id && v.title == data.title),1)
+      }else{
+        data.active = true
+        this.selTag.push(data)
+      }
+      // data.active = !data.active
+      this.tagColunm.forEach(v=>{
+        v.active = false
+        if(!v.tagList.some(val=>val.title==data.title)){
+          v.tagList.forEach(a=>{
+            if(a.active){
+              a.active=false
+              this.selTag.splice(this.selTag.findIndex(v=>v.id == a.id && v.title == a.title),1)
+            }
+          })
+        }
+      })
 
+      this.tagItem = null
+    }
+    this.sel$.next()
+  }
+  clearSel(){
+    this.tagItem = null
+    this.selTag = []
+    this.jsUtilSrv.loopTree(this.tagColunm, (v)=>{
+      if(v.active){
+        v.active = false
+      }
+    }, {mapObject:['tagList']})
+    this.sel$.next()
+  }
 }
